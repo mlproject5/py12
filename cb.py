2beef96dff48278b0a1935941caa90fb11b32067
mlproject5 <135617691+mlproject5@users.noreply.github.com>
2023-07-04 11:44:23 +0600

    Add files via upload

---
+++ cb.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import requests
+
+
+st.set_page_config(page_title='ChatBot', page_icon='chatbot.png', layout="centered", initial_sidebar_state="auto",
+                   menu_items=None)
+
+API_KEY = "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"
+API_URL = "https://api.openai.com/v1/chat/completions"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {API_KEY}"
+}
+
+hide_streamlit_style = """
+    <style>
+    footer {visibility: hidden;}
+    </style>
+    """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+def generate_response(message):
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message}
+        ]
+    }
+
+    response = requests.post(API_URL, headers=headers, json=data)
+    response.raise_for_status()
+    return response.json()["choices"][0]["message"]["content"]
+
+st.markdown(
+            "<center><h1 style='font-family: Comic Sans MS; font-weight: 600; font-size: 36px;'>ChatBot Demo</h1></center>",
+            unsafe_allow_html=True)
+
+user_input = st.text_area("User Question")
+
+if st.button("Send"):
+    response = generate_response(user_input)
+    st.text_area("ChatBot Answer", value=response, height=200, max_chars=None)
